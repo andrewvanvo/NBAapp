0a6e2f1c6c0862aae0dca0d8c9cc937a22f1e057
andrewvanvo <andrew.van.vo@gmail.com>
2022-03-23 22:20:45 -0400

    BrowserRouter

--- nba/src/App.js
+++ nba/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+//Modules
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+//CSS 
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className = 'App'>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        
+        </Routes>
+      </BrowserRouter>,
     </div>
-  );
+    
+  )
 }
 
 export default App;
