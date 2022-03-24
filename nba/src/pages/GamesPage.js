486281860da9532b8e3f902de107043d6a08f4be
andrewvanvo <andrew.van.vo@gmail.com>
2022-03-24 14:01:03 -0400

    DateProp state pass,pulled,decontructed,assigned.

--- nba/src/pages/GamesPage.js
+++ nba/src/pages/GamesPage.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 //changed format of module export. Wasn't playing well with the BrowserRouter for some reason
 
 
 //function GamesPage ({ gameDate }){
 function GamesPage(){
+
+    //Pulling state variable/Date picked from Homepage
+    const location = useLocation()
+    const {dateProp} = location.state
+    const day = dateProp.getDate()
+    const month = dateProp.getMonth() + 1
+    const year = dateProp.getFullYear()
+    
+
     // TEMP, need api call here
     //const [time, setTime] = useState(gameDate.time);
     // TEMP, need api call here
@@ -16,6 +25,8 @@ function GamesPage(){
     return (
         <>
             <p>GamesPage test</p>
+
+            {console.log(day,month,year)}
         </>
     )
 };
