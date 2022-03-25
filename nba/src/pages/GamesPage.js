ece340fbd4edc2029c02356b86a212d1a533b09b
andrewvanvo <andrew.van.vo@gmail.com>
2022-03-25 11:14:29 -0400

    GamesPage api calls the date and is no longer hardcoded, passes the useLocation prop vars

--- nba/src/pages/GamesPage.js
+++ nba/src/pages/GamesPage.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-// import { useHistory, useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 // Components 
 import GamesList from '../components/GamesList';
 
 function GamesPage() {
+
     const [games, setGames] = useState([]);
     // const history = useHistory();
     const options = {
@@ -15,8 +16,38 @@ function GamesPage() {
         }
     };
 
+    //Pulling state variable/Date picked from Homepage
+    const location = useLocation()
+    const { dateProp } = location.state
+    //date variables to use for api
+    let day = dateProp.getDate()
+    let month = dateProp.getMonth() + 1
+    let year = dateProp.getFullYear()
+
+    //converting date variables for dynamic passing into API
+    let dayStr
+    let monthStr
+    let yearStr = year.toString()
+
+    if (day >= 10){
+        dayStr = day.toString()
+       
+    } else {
+        dayStr = day.toString()
+        dayStr = '0'+ dayStr
+    }
+
+    if (month >= 10){
+        monthStr = month.toString()
+        
+    } else{
+        monthStr = month.toString()
+        monthStr = '0'+ monthStr
+    }
+
     const loadGames = async () => {
-        const response = await fetch('https://api-nba-v1.p.rapidapi.com/games?date=2022-02-12', options)
+
+        const response = await fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${yearStr}-${monthStr}-${dayStr}`, options)
         const data = await response.json();
         console.log(data.response);
         setGames(data.response);
